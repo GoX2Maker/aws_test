5cf1226309a205584d38aba3489fe86fdf338d72
GoX2Maker <131675046+GoX2Maker@users.noreply.github.com>
2024-08-18 10:31:33 +0900

    add input

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  TextEditingController controller = TextEditingController();
+  String sendData = 'Hello World';
+
+  @override
+  void initState() {
+    super.initState();
+    controller.text = sendData;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      home: SafeArea(
+        bottom: false,
+        child: Scaffold(
+          body: Column(
+            children: [
+              Center(
+                child: TextField(
+                  controller: controller,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter your name',
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      sendData = value;
+                    });
+                  },
+                ),
+              ),
+              TextButton(onPressed: () {}, child: const Text('Send Data')),
+            ],
+          ),
         ),
       ),
     );
